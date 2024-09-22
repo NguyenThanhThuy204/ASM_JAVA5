16141c0b3ab350f670d062ce947cc663e4918fd0
NguyenThanhThuy204 <tn77777204@gmail.com>
2024-09-22 13:37:42 +0700

    asmjava5

---
+++ PS19246_NguyenHoangPhuc_ASMFinal/PS19246_NguyenHoangPhuc_ASM/src/main/java/com/asm/Ps19246NguyenHoangPhucAsmApplication.java
@@ -0,0 +1,15 @@
+package com.asm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Ps19246NguyenHoangPhucAsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps19246NguyenHoangPhucAsmApplication.class, args);
+	}
+
+}
---
+++ PS19246_NguyenHoangPhuc_ASMFinal/PS19246_NguyenHoangPhuc_ASM/src/test/java/com/asm/Ps19246NguyenHoangPhucAsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps19246NguyenHoangPhucAsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
